3e8de40bf4f24e2cb14afbc399b29aa4a5221b6a
sayonara213 <79828791+sayonara213@users.noreply.github.com>
2022-04-08 14:12:52 +0800

    Create Const.java

---
+++ Const.java
@@ -0,0 +1,8 @@
+public class Const {
+    public static final String SWEET_ID = "idCandy";
+    public static final String SWEET_NAME = "nameCandy";
+    public static final String SWEET_SUGAR = "sugarCandy";
+    public static final String SWEET_WEIGHT = "weightCandy";
+    public static final String SWEET_QUAN = "quanCandy";
+    public static final String USER_TABLE = "sweets.candylist";
+}
